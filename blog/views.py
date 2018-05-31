3d8106efccf08369327c3d04a9ce3869629438c1
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-31 20:46:03 +0800

    Modify New/Edit, not store publicated_date;Add a draft list;Add a Publish Button

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,15 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk)
+
 def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{'post':post})
@@ -17,7 +26,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail',pk=post.pk)
     else:
@@ -31,7 +40,7 @@ def post_edit(request,pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail',pk=post.pk)
     else:

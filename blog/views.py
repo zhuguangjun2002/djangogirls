a9315fd4ea5c8235f40bb42fa5af053a717930d0
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-06-01 17:11:19 +0800

    add  function

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
 
+
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
-def post_publish(request,pk):
-    post = get_object_or_404(Post,pk=pk)
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     post.publish()
-    return redirect('post_detail',pk=pk)
+    return redirect('post_detail', pk=pk)
 
-def post_detail(request,pk):
-    post = get_object_or_404(Post,pk=pk)
-    return render(request,'blog/post_detail.html',{'post':post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -26,23 +33,30 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            #post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail',pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request,'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_edit(request,pk):
-    post = get_object_or_404(Post,pk=pk)
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST,instance=post)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            #post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail',pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request,'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_remove(reques, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
